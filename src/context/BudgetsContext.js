ec19b1c82856dde3be17e60c1e905b8087dd55b6
bengin34 <becaglar3434@gmail.com>
2023-02-14 19:43:45 +0300

    continue

---
+++ src/context/BudgetsContext.js
@@ -0,0 +1,12 @@
+import React, { useContext } from "react"
+
+const BudgetsContext = React.createContext()
+
+
+export function useBudgets(){
+    return useContext(BudgetsContext)
+}
+
+export const BudgetsProvider = ({ children }) => {
+    return children
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
+import { BudgetsProvider } from "./context/BudgetsContext"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <BudgetsProvider>
     <App />
+    </BudgetsProvider>
   </React.StrictMode>
 );
 
